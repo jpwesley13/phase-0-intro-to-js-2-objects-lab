b9c4ead5793174cfb2c5b8ef7b62189e0d62a113
Jared Patterson <jpwesley34@gmail.com>
2023-10-10 20:02:44 -0700

    destroys employees

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name : "CG",
+    streetAddress : "Castle"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,
+    [key]: value,
+};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const emp = {...employee[key]}
+        delete emp[key]
+        return emp
+    }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
